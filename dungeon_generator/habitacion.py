d4d70e9caefae5949f9becc449f077effbf8c533
Sriveros <sriveros.koenderink117@gmail.com>
2025-09-28 23:19:27 -0300

    Update habitacion.py

--- dungeon_generator/habitacion.py
+++ dungeon_generator/habitacion.py
@@ -2,41 +2,40 @@ from typing import Dict, Optional, Tuple
 from abc import ABC, abstractmethod
 from .objeto import Objeto
 
-
-
 class ContenidoHabitacion(ABC):
-   
-    
     @property
+    @abstractmethod
     def descripcion(self) -> str:
         raise NotImplementedError
     
     @property
+    @abstractmethod
     def tipo(self) -> str:
         raise NotImplementedError
     
+    @abstractmethod
     def interactuar(self, explorador) -> str:
         raise NotImplementedError
-
+    
+    @abstractmethod
+    def to_dict(self) -> dict:
+        """Convierte el contenido a diccionario para serializacion"""
+        raise NotImplementedError
 
 class Habitacion:
-    
-    
     def __init__(self, id: int, x: int, y: int, inicial: bool = False):
         self.id = id
         self.x = x
         self.y = y
         self.inicial = inicial
         self.contenido: Optional[ContenidoHabitacion] = None
-        self.conexiones: Dict[str, 'Habitacion'] = {}  # 4 puntos cardinales: norte, sur, este, oeste
+        self.conexiones: Dict[str, 'Habitacion'] = {}
         self.visitada = False
     
     def agregar_conexion(self, direccion: str, habitacion: 'Habitacion'):
-       
         self.conexiones[direccion] = habitacion
     
     def obtener_direcciones_disponibles(self) -> list[str]:
-        
         return list(self.conexiones.keys())
     
     def __str__(self) -> str:
@@ -46,13 +45,17 @@ class Habitacion:
         return f"Habitacion {self.id} ({self.x},{self.y}) - {estado} - {visitada} - {contenido}"
     
     def to_dict(self) -> dict:
-           
-        return {
+        """Convierte la habitacion a diccionario para serializacion"""
+        datos = {
             "id": self.id,
             "x": self.x,
             "y": self.y,
             "inicial": self.inicial,
             "visitada": self.visitada,
-            
-
+            "contenido": None
         }
+        
+        if self.contenido:
+            datos["contenido"] = self.contenido.to_dict()
+        
+        return datos
