d73ec5b27e9e689a38f970517ae86c53183f3bdf
Sriveros <sriveros.koenderink117@gmail.com>
2025-10-04 14:59:01 -0300

    Update __init__.py

--- dungeon_generator/__init__.py
+++ dungeon_generator/__init__.py
@@ -1,4 +1,12 @@
-"""para generacion de mapas de dungeon"""
+"""Generador de Mapas de Dungeon - Coliseo de Campeones"""
 
+from .habitacion import Habitacion, ContenidoHabitacion
+from .mapa import Mapa
+from .explorador import Explorador
+from .objeto import Objeto
+from .contenido import Tesoro, Monstruo, Jefe
+from .eventos import Evento, GritoMultitud, TrampaGladiadores, FuenteCampeones, PortalDestino, FantasmaCampeon
+from .visualizador import Visualizador
+from .serializador import Serializador
 
 __version__ = "0.1.0"
